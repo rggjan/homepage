7237dd813935f4f412d9b2c5f31f52ad5d1bd2c5
mkhan3189 <mkhan3189@gmail.com>
2013-10-19 19:16:34 +1100

    v9.29 freleas

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
-$currentVersion = "9.28";
-$releaseDate ="18-10-2013 1015hrs";
+$currentVersion = "9.29";
+$releaseDate ="19-10-2013 1914hrs";
 $major = substr($currentVersion, 0, 1);
 $minor = substr($currentVersion, 2, 2);
 ?>
