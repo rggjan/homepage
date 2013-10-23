0752d6fcd18cbf9214b12d9336d0c9d80a183599
mkhan3189 <mkhan3189@gmail.com>
2013-10-23 18:44:59 +1100

    v9.34 fix

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
-$currentVersion = "9.33";
-$releaseDate ="23-10-2013 1736hrs";
+$currentVersion = "9.34";
+$releaseDate ="23-10-2013 1844hrs";
 $major = substr($currentVersion, 0, 1);
 $minor = substr($currentVersion, 2, 2);
 ?>
