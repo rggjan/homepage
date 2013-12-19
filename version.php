46cbe3ff41e8b584f3857b4282e45d562c437b63
mkhan3189 <mkhan3189@gmail.com>
2013-12-20 00:24:19 +1100

    v9.42 release

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
-$currentVersion = "9.41";
-$releaseDate ="18-12-2013 1751hrs";
+$currentVersion = "9.42";
+$releaseDate ="20-12-2013 0023hrs";
 $major = substr($currentVersion, 0, 1);
 $minor = substr($currentVersion, 2, 2);
 ?>
