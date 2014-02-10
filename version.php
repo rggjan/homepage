06a38d51e145ece6c2761be7bfef16e2b353288f
mkhan <mkhan@allhomes.com.au>
2014-02-10 12:46:44 +1100

    v9.51 release

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
-$currentVersion = "9.50";
-$releaseDate ="04-02-2014 0822hrs";
+$currentVersion = "9.51";
+$releaseDate ="10-02-2014 1244hrs";
 $major = substr($currentVersion, 0, 1);
 $minor = substr($currentVersion, 2, 2);
 ?>
