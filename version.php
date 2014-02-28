47ec63c45afe8457a832259d946dbdcde82f7f99
mkhan <mkhan@allhomes.com.au>
2014-02-28 12:26:19 +1100

    release date fix

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 $currentVersion = "9.57";
-$releaseDate ="28-02-2014 1217hrs";
+$releaseDate ="28-02-2014 1220hrs";
 $major = substr($currentVersion, 0, 1);
 $minor = substr($currentVersion, 2, 2);
 ?>
