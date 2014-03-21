8076cde588ad5ad4885e09073a3fa98661436f70
mkhan3189 <mkhan3189@gmail.com>
2014-03-21 21:34:28 +1100

    v9.58 release

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
-$currentVersion = "9.57";
-$releaseDate ="28-02-2014 1220hrs";
+$currentVersion = "9.58";
+$releaseDate ="14-03-2014 0950hrs";
 $major = substr($currentVersion, 0, 1);
 $minor = substr($currentVersion, 2, 2);
 ?>
