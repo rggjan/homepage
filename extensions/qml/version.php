7a8d2e1554834e3bdba91dae048e89934189f2a5
mkhan3189 <mkhan3189@gmail.com>
2014-03-23 17:27:57 +1100

    default version

--- extensions/qml/version.php
+++ extensions/qml/version.php
@@ -1,6 +1,6 @@
 <?php
 $currentVersion = "0.0";
-$releaseDate ="00-00-0000 0000hrs";
+$releaseDate ="01-01-2012 0000hrs";
 $major = substr($currentVersion, 0, 1);
 $minor = substr($currentVersion, 2, 2);
 ?>
