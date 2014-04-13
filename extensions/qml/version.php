16675ab19a31ec7d4e25282d6f098a2caa29ebdb
mkhan3189 <mkhan3189@gmail.com>
2014-04-13 21:21:24 +1000

    v1.2 release

--- extensions/qml/version.php
+++ extensions/qml/version.php
@@ -1,6 +1,6 @@
 <?php
-$currentVersion = "1.1";
-$releaseDate ="06-04-2014 1700hrs";
+$currentVersion = "1.2";
+$releaseDate ="13-04-2014 2120hrs";
 $major = substr($currentVersion, 0, 1);
 $minor = substr($currentVersion, 2, 2);
 ?>
