b03721d89ebee916701179962b7fd772dbfb9442
Majid Khan <mkhan3189@gmail.com>
2014-05-11 11:04:22 +1000

    v9.72 release and old release note

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
-$currentVersion = "9.70";
-$releaseDate ="17-04-2014 1832hrs";
+$currentVersion = "9.72";
+$releaseDate ="25-04-2014 1619hrs";
 $major = substr($currentVersion, 0, 1);
 $minor = substr($currentVersion, 2, 2);
 ?>
