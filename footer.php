8bc78d0eb69f69d20288fabafcf16df002603c7b
mkhan <mkhan@allhomes.com.au>
2014-05-19 14:25:21 +1000

    footer update

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
         <br/>
         <a href="/licence.php">Licence</a>&nbsp;|&nbsp;
         <a href="https://github.com/easylogging/">Github</a>&nbsp;|&nbsp;
-        <a href="http://www.icplusplus.com">icplusplus.com</a>
+        <a href="https://allahu-ahad.org/">Allahu Ahad</a>
         <br/>
         <br/>
         This is open source site <a href="https://github.com/easylogging/homepage/">hosted on github</a>
