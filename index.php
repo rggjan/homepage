2d5669a89c5c98736d66a05974db37b618a123b9
mkhan <mkhan@allhomes.com.au>
2014-08-14 09:49:11 +1000

    c++11 info

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 ?>
 <pre><span class="vinfo">Current version <?php print "v$currentVersion ($releaseDate)"; ?> </span></pre>
 <p>
-Single header only, extremely light-weight high performance logging library for C++ applications
+Single header only, extremely light-weight high performance logging library for C++11 (or higher) applications.<br/>For lower version of C++ (non-C++11), please consider using <a href='https://github.com/easylogging/easyloggingpp/tree/v8.91' target='_blank'>Easylogging++ v8.91</a>.
 </p><p>
 The strengths of Easylogging++ are its speed and simplicity. It is extremely light-weight, robust, fast performing, thread and type safe and consists of many built-in features. It fits in small to medium sized projects, it provides ability to write logs in your own customized format and also provide support for logging your classes, third-party libraries, STL and third-party containers etc.
 </p>
 <p>
-You may want to have a look at <a href='https://github.com/mkhan3189/project-islam'>Project Islam</a> to observe complete use of this library. You may also wish to have a look at <a href='http://easylogging.org/sample.log'>sample log file</a> generated from this project.
+You may want to have a look at <a href='https://github.com/mkhan3189/project-islam' target='_blank'>this project</a> to observe complete use of this library. You may also wish to have a look at <a href='http://easylogging.org/sample.log'>sample log file</a> generated from this project.
 </p>
 <table width="100%">
 <tr>
